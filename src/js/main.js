d9c7eaade354ac849f0d6f6c538d1394d608632e
Jamie Waters <jwaters627@gmail.com>
2016-05-03 22:48:04 -0400

    adding overlay for e mail capture

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-// testing //
+
+// $('#no').on('click', function () {
+// 	$('#not21').css({display: 'block'})
+// });
+
+//  $('#yes').on('click', function () {
+//     $('#gatekeeper').fadeOut(1000);
+//     $('#entire').css({opacity: 1});
+// });
+
+$(function(){  
+  $('#emailcapture').fadeIn(2000);
+});
+
+$('#emailcapture').on('click', function() {
+	$('#emailcapture').fadeOut(2000);
+
+});
