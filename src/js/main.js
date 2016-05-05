ace0ee5b71a880086583db9ec06183d8e917014c
Christina <cwiech@alumni.nd.edu>
2016-05-05 12:45:12 -0400

    added for loop

--- src/js/main.js
+++ src/js/main.js
@@ -42,11 +42,14 @@ var trips = [{
     lodging: "camping",
 }, ];
 
+newobj = {};
+
 var selections = function() {
-    newobj = {};
+    
     document.addEventListener("click", function(e) {
         if (e.target.id == "domestic" || e.target.id == "international") {
             newobj.distance = e.target.id
+
         } else if (e.target.id == "urban" || e.target.id == "rural") {
             newobj.remoteness = e.target.id
         }
@@ -81,7 +84,45 @@ var selections = function() {
 }
 selections();
 
+// Submission
 
+var submission = function() {
+    
+    document.addEventListener("click", function(e) {
+        if(e.target.id === "submitted"){
+        alert('here');
+        console.log(newobj);
+
+
+        // for (i = 0; i < trips.length; i++) {
+        //     if (trips[i].distance !== newobj.distance) {
+        //         continue;
+        //     } else if (trips[i].remoteness !== newobj.remoteness) {
+        //         continue;
+        //     } else if (trips[i].landscape !== newobj.landscape) {
+        //         continue;
+        //     } else if (trips[i].price !== newobj.price) {
+        //         continue;
+        //     } else if (trips[i].rigor !== newobj.rigor) {
+        //         continue;
+        //     } else if (trips[i].tastes !== newobj.tastes) {
+        //         continue;
+        //     } else if (trips[i].interests !== newobj.interests) {
+        //         continue;
+        //     } else if (trips[i].climate !== newobj.climate) {
+        //         continue;
+        //     } else if (trips[i].companions !== newobj.companions) {
+        //         continue;
+        //     } else {
+        //         console.log(trips[i].name);
+        //     }
+        // };
+
+    });
+    }
+}
+
+submission();
 
 
 
